f4ebf11e022fe2dd7580311fb46e188cfaf2121f
AyabulelaSityata <sityataayabulela@gmail.com>
2023-08-14 14:42:16 +0200

    controller - user

--- api/model/index.js
+++ api/model/index.js
@@ -1,1 +1,8 @@
-// Index
+const Users = require('./Users')
+const Orders = require('./Orders')
+const Books = require('./Books')
+const BookAuthors = require('./BookAuthors')
+// Export all objects
+module.exports = {
+    users: new Users(),
+}
