5f415123116e9bf4e3a933df774c9241537e95b1
AyabulelaSityata <sityataayabulela@gmail.com>
2023-08-19 10:38:00 +0200

    Ke saturday

---
+++ challenger_gui/src/components/FooterComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="footer">
+            <p>&COPY; 2023 - All Rights Reserved</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+
+</style>
---
+++ challenger_gui/src/components/NavBar.vue
@@ -0,0 +1,75 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" to="/home">
+                <img class="logo rounded-circle" id="image" src="https://i.postimg.cc/7Lv7DRLC/a.png" alt="LOGO">
+            </router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/home">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">About</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/products">Products</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/contact">Contact Us</router-link>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nav {
+    z-index: 100;
+    color: white !important;
+    background-image: linear-gradient(135deg, black, rgb(180, 180, 180)) !important;
+    box-shadow: 0 0 4px black;
+    padding: 20px;
+}
+
+#image {
+    height: 4rem;
+    width: 4rem;
+}
+
+a:nth-child(1) {
+    display: flex;
+    justify-content: flex-start;
+}</style>
